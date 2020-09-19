654a674111ea538df172f27d4f67d4c679745afa
Evan <evann710@gmail.com>
2020-09-19 13:31:32 -0400

    It works now, client and server

--- server/server.js
+++ server/server.js
@@ -1,2 +1,10 @@
-// This is the server
-// TODO
+var express = require('express');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/../client/dist"));
+var port = process.env.PORT || 8080;
+var hostname = 'localhost';
+
+app.listen(port, hostname, () => {
+   console.log(`Server running at http://${hostname}:${port}/`);
+ });
