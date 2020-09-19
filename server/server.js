ae1f9dc36d13daef864303b0c5f5ab36babfbd1f
Evan <evann710@gmail.com>
2020-09-19 13:59:28 -0400

    Connected to database

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
-var express = require('express');
-var serveStatic = require('serve-static');
+var express = require("express");
+var serveStatic = require("serve-static");
+const sqlite = require("sqlite")
 app = express();
 app.use(serveStatic(__dirname + "/../client/dist"));
 var port = process.env.PORT || 8080;
-var hostname = 'localhost';
+var hostname = "localhost";
 
 app.listen(port, hostname, () => {
-   console.log(`Server running at http://${hostname}:${port}/`);
- });
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+(async () => {
+  // open the database
+  const db = await sqlite.open({
+    filename: "/tmp/database.db",
+    driver: sqlite.Database,
+  });
+})().then(function(value) {
+    console.log("Async works!", value);
+}).catch(function(err) {
+    console.log("Error!", err)
+});
